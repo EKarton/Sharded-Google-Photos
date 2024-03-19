a99c8b6aeabce4c87673db87c92931ab5cc70111
Emilio K <emiliokartono@gmail.com>
2024-03-18 23:34:03 -0700

    feat: create fake gphotos client

--- sharded_google_photos/shared/testing/fake_gphotos_client.py
+++ sharded_google_photos/shared/testing/fake_gphotos_client.py
@@ -3,6 +3,210 @@ import uuid
 from sharded_google_photos.shared.gphotos_client import GPhotosClient
 
 
+class FakeItemsRepository:
+    def __init__(self):
+        self.album_id_to_album = {}
+        self.album_id_to_media_item_ids = {}
+        self.album_id_to_accessible_client_ids = {}
+        self.album_id_to_owned_client_id = {}
+        
+        self.share_token_to_album_id = {}
+
+        self.media_item_id_to_media_item = {}
+        self.media_item_ids_to_owned_client_id = {}
+        self.upload_tokens_to_file_name = {}
+        self.media_item_ids_to_accessible_client_ids = {}
+
+    def list_shared_albums(self, client_id):
+        def is_allowed(album):
+            is_shared = album["shareInfo"] is not None
+            is_accessible = (
+                client_id in self.album_id_to_accessible_client_ids[album["id"]]
+            )
+            return is_shared and is_accessible
+
+        return list(filter(is_allowed, self.album_id_to_album.values()))
+
+    def list_unshared_albums(self, client_id):
+        def is_allowed(album):
+            is_shared = album["shareInfo"] is None
+            is_accessible = (
+                client_id in self.album_id_to_accessible_client_ids[album["id"]]
+            )
+            return is_shared and is_accessible
+
+        return list(filter(is_allowed, self.album_id_to_album.values()))
+
+    def create_album(self, client_id, album_name):
+        new_album_id = str(uuid.uuid4())
+        new_album = {
+            "id": new_album_id,
+            "title": album_name,
+            "productUrl": f"http://google.com/albums/{new_album_id}",
+            "isWriteable": True,
+            "shareInfo": None,
+            "mediaItemsCount": 0,
+            "coverPhotoBaseUrl": None,
+            "coverPhotoMediaItemId": None,
+        }
+        self.album_id_to_album[new_album_id] = new_album
+        self.album_id_to_media_item_ids[new_album_id] = set()
+        self.album_id_to_accessible_client_ids[new_album_id] = set([client_id])
+        self.album_id_to_owned_client_id[new_album_id] = client_id
+
+        return {
+            "id": new_album["id"],
+            "title": new_album["title"],
+            "productUrl": new_album["productUrl"],
+            "isWriteable": new_album["isWriteable"],
+        }
+
+    def share_album(
+        self, client_id, album_id, is_collaborative=False, is_commentable=False
+    ):
+        if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+            raise Exception("Cannot share album that it does not own")
+
+        share_token = str(uuid.uuid4())
+        share_info = {
+            "sharedAlbumOptions": {
+                "isCollaborative": is_collaborative,
+                "isCommentable": is_commentable,
+            },
+            "shareableUrl": f"http://google.com/shared-albums/{album_id}",
+            "shareToken": share_token,
+            "isJoined": True,
+            "isOwned": True,
+            "isJoinable": True,
+        }
+        self.album_id_to_album[album_id]["shareInfo"] = share_info
+        self.share_token_to_album_id[share_token] = album_id
+
+        return share_info
+
+    def join_album(self, client_id, share_token):
+        album_id = self.share_token_to_album_id[share_token]
+        self.album_id_to_accessible_client_ids[album_id].add(client_id)
+
+    def unshare_album(self, client_id, album_id):
+        if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+            raise Exception("Cannot unshare album that it does not own")
+
+        self.album_id_to_accessible_client_ids[album_id] = set()
+        self.album_id_to_album[album_id]["shareInfo"] = None
+
+    def add_photos_to_album(self, client_id, album_id, media_item_ids):
+        for media_id in media_item_ids:
+            if client_id not in self.media_item_ids_to_accessible_client_ids[media_id]:
+                raise Exception("Cannot put someone's media item into album")
+
+            self.album_id_to_media_item_ids[album_id].add(media_id)
+
+    def remove_photos_from_album(self, client_id, album_id, media_item_ids):
+        if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+            raise Exception("Cannot remove photos from album it did not join")
+
+        if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+            raise Exception("Cannot remove photos from album it did not join")
+
+        for media_id in media_item_ids:
+            if client_id not in self.media_item_ids_to_accessible_client_ids[media_id]:
+                raise Exception("Cannot remove someone else's photos from album")
+
+            self.album_id_to_media_item_ids[album_id].remove(media_id)
+
+    def add_uploaded_photos_to_gphotos(self, client_id, upload_tokens, album_id=None):
+        new_media_items_results = []
+        for upload_token in upload_tokens:
+            new_media_item_id = str(uuid.uuid4())
+
+            if album_id is not None:
+                self.album_id_to_media_item_ids[album_id].add(new_media_item_id)
+
+            if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+                raise Exception("Cannot add uploaded photos to inaccessible album")
+
+            new_media_item = {
+                "id": new_media_item_id,
+                "description": "New photo",
+                "productUrl": f"http://google.com/photos/{new_media_item_id}",
+                "baseUrl": f"http://google.com/photos/{new_media_item_id}",
+                "mimeType": "jpeg",
+                "mediaMetadata": {
+                    "creationTime": "2014-10-02T15:01:23Z",
+                    "width": "200px",
+                    "height": "300px",
+                    "photo": {
+                        "cameraMake": "IPhone",
+                        "cameraModel": "14 Pro",
+                        "focalLength": 50,
+                        "apertureFNumber": 1.4,
+                        "isoEquivalent": 400,
+                        "exposureTime": "0.005s",
+                    },
+                },
+                "contributorInfo": {
+                    "profilePictureBaseUrl": "http://google.com/profile/1",
+                    "displayName": "Bob Smith",
+                },
+                "filename": self.upload_tokens_to_file_name[upload_token],
+            }
+
+            self.media_item_id_to_media_item[new_media_item_id] = new_media_item
+            self.media_item_ids_to_accessible_client_ids[new_media_item_id] = set(
+                [client_id]
+            )
+
+            new_media_items_results.append(
+                {
+                    "uploadToken": upload_token,
+                    "status": {"code": 200, "message": "Success", "details": []},
+                    "mediaItem": new_media_item,
+                }
+            )
+
+        return {"newMediaItemResults": new_media_items_results}
+
+    def upload_photo(self, client_id, photo_file_path, file_name):
+        upload_token = str(uuid.uuid4())
+        self.upload_tokens_to_file_name[upload_token] = file_name
+        return upload_token
+
+    def search_for_media_items(
+        self, client_id, album_id=None, filters=None, order_by=None
+    ):
+        if album_id is not None:
+            if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+                raise Exception("Cannot search in inaccessible album")
+
+            return [
+                self.media_item_id_to_media_item[media_item_id]
+                for media_item_id in self.album_id_to_media_item_ids[album_id]
+            ]
+        else:
+            def is_valid(item):
+                is_owned = client_id == self.media_item_ids_to_owned_client_id[item["id"]]
+                return is_owned
+
+            all_media_items = list(self.media_item_id_to_media_item.values())
+            return list(filter(is_valid, all_media_items))
+
+    def update_album(
+        self, client_id, album_id, new_title=None, new_cover_media_item_id=None
+    ):
+        if client_id != self.album_id_to_owned_client_id[album_id]:
+            raise Exception("Cannot update album it does not own")
+
+        album_info = self.album_id_to_album[album_id]
+        if new_title is not None:
+            album_info["title"] = new_title
+        if new_cover_media_item_id is not None:
+            album_info["coverPhotoMediaItemId"] = new_cover_media_item_id
+            album_info["coverPhotoBaseUrl"] = (
+                f"http://google.com/photos/{new_cover_media_item_id}"
+            )
+
+
 class FakeGPhotosClient(GPhotosClient):
     def __init__(self):
         self.is_authenticated = False
@@ -24,7 +228,11 @@ class FakeGPhotosClient(GPhotosClient):
 
     def list_shared_albums(self, exclude_non_app_created_data=False):
         self.__check_authentication__()
-        return list(filter(lambda x: x["shareInfo"] is not None, self.album_id_to_album.values()))
+        return list(
+            filter(
+                lambda x: x["shareInfo"] is not None, self.album_id_to_album.values()
+            )
+        )
 
     def list_albums(self, exclude_non_app_created_data=False):
         self.__check_authentication__()
@@ -77,7 +285,7 @@ class FakeGPhotosClient(GPhotosClient):
 
     def unshare_album(self, album_id):
         self.__check_authentication__()
-        raise NotImplementedError()
+        self.album_id_to_album[album_id]["shareInfo"] = None
 
     def add_photos_to_album(self, album_id, media_item_ids):
         self.__check_authentication__()
--- tests/gphotos_cleaner/test_gphotos_cleaner.py
+++ tests/gphotos_cleaner/test_gphotos_cleaner.py
@@ -9,7 +9,7 @@ class GPhotosClientTests(unittest.TestCase):
         self.client_1 = FakeGPhotosClient()
         self.client_1.authenticate()
 
-    def test_mark_unalbumed_photos_to_trash_no_trash_album_puts_non_shared_photos_to_trash(
+    def test_mark_unalbumed_photos_to_trash_photos_in_shared_album_puts_non_shared_photos_to_trash(
         self,
     ):
         # Test setup: Add four photos in client 1 with two of them in shared album A
@@ -27,18 +27,58 @@ class GPhotosClientTests(unittest.TestCase):
         cleaner.mark_unalbumed_photos_to_trash()
 
         # Assertions: Check that u3 and u4 are in the trash
-        trash_id = next(
-            filter(lambda x: x["title"] == "Trash", self.client_1.list_albums()), None
-        )["id"]
-        media_items = self.client_1.search_for_media_items(trash_id)
-        media_item_ids_in_trash = set([media_item["id"] for media_item in media_items])
-        expected_media_item_ids = set(
-            [upload["mediaItem"]["id"] for upload in upload_2["newMediaItemResults"]]
-        )
-        self.assertEqual(len(media_items), 2)
+        media_item_ids_in_trash = self.__get_media_item_ids_in_trash__(self.client_1)
+        expected_media_item_ids = self.__get_media_ids_from_uploaded_files__(upload_2)
         self.assertEqual(media_item_ids_in_trash, expected_media_item_ids)
 
-    def test_mark_unalbumed_photos_to_trash_photos_in_albums_does_not_put_albumed_photos_to_trash(
+    def test_mark_unalbumed_photos_to_trash_photos_in_unshared_album_puts_photos_to_trash(
         self,
     ):
-        pass
+        # Test setup: Add four photos to an unshared album
+        u1 = self.client_1.upload_photo("A/1.jpg", "1.jpg")
+        u2 = self.client_1.upload_photo("A/2.jpg", "2.jpg")
+        u3 = self.client_1.upload_photo("A/3.jpg", "3.jpg")
+        u4 = self.client_1.upload_photo("A/4.jpg", "4.jpg")
+        a1 = self.client_1.create_album("A")["id"]
+        upload_1 = self.client_1.add_uploaded_photos_to_gphotos([u1, u2, u3, u4], a1)
+
+        # Act:
+        cleaner = GPhotosCleaner(self.client_1)
+        cleaner.mark_unalbumed_photos_to_trash()
+
+        media_item_ids_in_trash = self.__get_media_item_ids_in_trash__(self.client_1)
+        expected_media_item_ids = self.__get_media_ids_from_uploaded_files__(upload_1)
+        self.assertEqual(media_item_ids_in_trash, expected_media_item_ids)
+
+    def test_mark_unalbumed_photos_to_trash_photos_in_no_albums_puts_photos_to_trash(
+        self,
+    ):
+        # Test setup: Add four photos to an unshared album
+        u1 = self.client_1.upload_photo("A/1.jpg", "1.jpg")
+        u2 = self.client_1.upload_photo("A/2.jpg", "2.jpg")
+        u3 = self.client_1.upload_photo("A/3.jpg", "3.jpg")
+        u4 = self.client_1.upload_photo("A/4.jpg", "4.jpg")
+        upload_1 = self.client_1.add_uploaded_photos_to_gphotos([u1, u2, u3, u4])
+
+        # Act:
+        cleaner = GPhotosCleaner(self.client_1)
+        cleaner.mark_unalbumed_photos_to_trash()
+
+        media_item_ids_in_trash = self.__get_media_item_ids_in_trash__(self.client_1)
+        expected_media_item_ids = self.__get_media_ids_from_uploaded_files__(upload_1)
+        self.assertEqual(media_item_ids_in_trash, expected_media_item_ids)
+
+    def __get_media_item_ids_in_trash__(self, client):
+        albums = client.list_albums()
+        trash_id = next(x["id"] for x in albums if x["title"] == "Trash")
+        media_items = client.search_for_media_items(trash_id)
+
+        return set([media_item["id"] for media_item in media_items])
+
+    def __get_media_ids_from_uploaded_files__(self, upload_results):
+        return set(
+            [
+                upload["mediaItem"]["id"]
+                for upload in upload_results["newMediaItemResults"]
+            ]
+        )
