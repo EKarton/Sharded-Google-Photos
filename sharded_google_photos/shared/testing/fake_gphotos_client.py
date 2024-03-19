4f1bf73ec2bf9cc0b1ba92681cf17d38bfc3e087
Emilio K <emiliokartono@gmail.com>
2024-03-18 23:49:07 -0700

    renamed vars

--- sharded_google_photos/shared/testing/fake_gphotos_client.py
+++ sharded_google_photos/shared/testing/fake_gphotos_client.py
@@ -5,37 +5,37 @@ from sharded_google_photos.shared.gphotos_client import GPhotosClient
 
 class FakeItemsRepository:
     def __init__(self):
-        self.album_id_to_album = {}
-        self.album_id_to_media_item_ids = {}
-        self.album_id_to_accessible_client_ids = {}
-        self.album_id_to_owned_client_id = {}
+        self.__album_id_to_album = {}
+        self.__album_id_to_media_item_ids = {}
+        self.__album_id_to_accessible_client_ids = {}
+        self.__album_id_to_owned_client_id = {}
 
-        self.share_token_to_album_id = {}
+        self.__share_token_to_album_id = {}
 
-        self.media_item_id_to_media_item = {}
-        self.media_item_ids_to_owned_client_id = {}
-        self.upload_tokens_to_file_name = {}
-        self.media_item_ids_to_accessible_client_ids = {}
+        self.__media_item_id_to_media_item = {}
+        self.__media_item_ids_to_owned_client_id = {}
+        self.__upload_tokens_to_file_name = {}
+        self.__media_item_ids_to_accessible_client_ids = {}
 
     def list_shared_albums(self, client_id):
         def is_allowed(album):
             is_shared = album["shareInfo"] is not None
             is_accessible = (
-                client_id in self.album_id_to_accessible_client_ids[album["id"]]
+                client_id in self.__album_id_to_accessible_client_ids[album["id"]]
             )
             return is_shared and is_accessible
 
-        return list(filter(is_allowed, self.album_id_to_album.values()))
+        return list(filter(is_allowed, self.__album_id_to_album.values()))
 
     def list_unshared_albums(self, client_id):
         def is_allowed(album):
             is_shared = album["shareInfo"] is None
             is_accessible = (
-                client_id in self.album_id_to_accessible_client_ids[album["id"]]
+                client_id in self.__album_id_to_accessible_client_ids[album["id"]]
             )
             return is_shared and is_accessible
 
-        return list(filter(is_allowed, self.album_id_to_album.values()))
+        return list(filter(is_allowed, self.__album_id_to_album.values()))
 
     def create_album(self, client_id, album_name):
         new_album_id = str(uuid.uuid4())
@@ -49,10 +49,10 @@ class FakeItemsRepository:
             "coverPhotoBaseUrl": None,
             "coverPhotoMediaItemId": None,
         }
-        self.album_id_to_album[new_album_id] = new_album
-        self.album_id_to_media_item_ids[new_album_id] = set()
-        self.album_id_to_accessible_client_ids[new_album_id] = set([client_id])
-        self.album_id_to_owned_client_id[new_album_id] = client_id
+        self.__album_id_to_album[new_album_id] = new_album
+        self.__album_id_to_media_item_ids[new_album_id] = set()
+        self.__album_id_to_accessible_client_ids[new_album_id] = set([client_id])
+        self.__album_id_to_owned_client_id[new_album_id] = client_id
 
         return {
             "id": new_album["id"],
@@ -64,7 +64,7 @@ class FakeItemsRepository:
     def share_album(
         self, client_id, album_id, is_collaborative=False, is_commentable=False
     ):
-        if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+        if client_id not in self.__album_id_to_accessible_client_ids[album_id]:
             raise Exception("Cannot share album that it does not own")
 
         share_token = str(uuid.uuid4())
@@ -79,41 +79,47 @@ class FakeItemsRepository:
             "isOwned": True,
             "isJoinable": True,
         }
-        self.album_id_to_album[album_id]["shareInfo"] = share_info
-        self.share_token_to_album_id[share_token] = album_id
+        self.__album_id_to_album[album_id]["shareInfo"] = share_info
+        self.__share_token_to_album_id[share_token] = album_id
 
         return share_info
 
     def join_album(self, client_id, share_token):
-        album_id = self.share_token_to_album_id[share_token]
-        self.album_id_to_accessible_client_ids[album_id].add(client_id)
+        album_id = self.__share_token_to_album_id[share_token]
+        self.__album_id_to_accessible_client_ids[album_id].add(client_id)
 
     def unshare_album(self, client_id, album_id):
-        if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+        if client_id not in self.__album_id_to_accessible_client_ids[album_id]:
             raise Exception("Cannot unshare album that it does not own")
 
-        self.album_id_to_accessible_client_ids[album_id] = set()
-        self.album_id_to_album[album_id]["shareInfo"] = None
+        self.__album_id_to_accessible_client_ids[album_id] = set()
+        self.__album_id_to_album[album_id]["shareInfo"] = None
 
     def add_photos_to_album(self, client_id, album_id, media_item_ids):
         for media_id in media_item_ids:
-            if client_id not in self.media_item_ids_to_accessible_client_ids[media_id]:
+            if (
+                client_id
+                not in self.__media_item_ids_to_accessible_client_ids[media_id]
+            ):
                 raise Exception("Cannot put someone's media item into album")
 
-            self.album_id_to_media_item_ids[album_id].add(media_id)
+            self.__album_id_to_media_item_ids[album_id].add(media_id)
 
     def remove_photos_from_album(self, client_id, album_id, media_item_ids):
-        if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+        if client_id not in self.__album_id_to_accessible_client_ids[album_id]:
             raise Exception("Cannot remove photos from album it did not join")
 
-        if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+        if client_id not in self.__album_id_to_accessible_client_ids[album_id]:
             raise Exception("Cannot remove photos from album it did not join")
 
         for media_id in media_item_ids:
-            if client_id not in self.media_item_ids_to_accessible_client_ids[media_id]:
+            is_accessible = (
+                client_id in self.__media_item_ids_to_accessible_client_ids[media_id]
+            )
+            if not is_accessible:
                 raise Exception("Cannot remove someone else's photos from album")
 
-            self.album_id_to_media_item_ids[album_id].remove(media_id)
+            self.__album_id_to_media_item_ids[album_id].remove(media_id)
 
     def add_uploaded_photos_to_gphotos(self, client_id, upload_tokens, album_id=None):
         new_media_items_results = []
@@ -121,9 +127,9 @@ class FakeItemsRepository:
             new_media_item_id = str(uuid.uuid4())
 
             if album_id is not None:
-                if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+                if client_id not in self.__album_id_to_accessible_client_ids[album_id]:
                     raise Exception("Cannot add uploaded photos to inaccessible album")
-                self.album_id_to_media_item_ids[album_id].add(new_media_item_id)
+                self.__album_id_to_media_item_ids[album_id].add(new_media_item_id)
 
             new_media_item = {
                 "id": new_media_item_id,
@@ -148,14 +154,14 @@ class FakeItemsRepository:
                     "profilePictureBaseUrl": "http://google.com/profile/1",
                     "displayName": "Bob Smith",
                 },
-                "filename": self.upload_tokens_to_file_name[upload_token],
+                "filename": self.__upload_tokens_to_file_name[upload_token],
             }
 
-            self.media_item_id_to_media_item[new_media_item_id] = new_media_item
-            self.media_item_ids_to_accessible_client_ids[new_media_item_id] = set(
+            self.__media_item_id_to_media_item[new_media_item_id] = new_media_item
+            self.__media_item_ids_to_accessible_client_ids[new_media_item_id] = set(
                 [client_id]
             )
-            self.media_item_ids_to_owned_client_id[new_media_item_id] = client_id
+            self.__media_item_ids_to_owned_client_id[new_media_item_id] = client_id
 
             new_media_items_results.append(
                 {
@@ -169,39 +175,37 @@ class FakeItemsRepository:
 
     def upload_photo(self, client_id, photo_file_path, file_name):
         upload_token = str(uuid.uuid4())
-        self.upload_tokens_to_file_name[upload_token] = file_name
+        self.__upload_tokens_to_file_name[upload_token] = file_name
         return upload_token
 
     def search_for_media_items(
         self, client_id, album_id=None, filters=None, order_by=None
     ):
         if album_id is not None:
-            if client_id not in self.album_id_to_accessible_client_ids[album_id]:
+            if client_id not in self.__album_id_to_accessible_client_ids[album_id]:
                 raise Exception("Cannot search in inaccessible album")
 
             return [
-                self.media_item_id_to_media_item[media_item_id]
-                for media_item_id in self.album_id_to_media_item_ids[album_id]
+                self.__media_item_id_to_media_item[media_item_id]
+                for media_item_id in self.__album_id_to_media_item_ids[album_id]
             ]
         else:
 
             def is_valid(media_item):
-                is_owned = (
-                    client_id
-                    == self.media_item_ids_to_owned_client_id[media_item["id"]]
-                )
+                a = self.__media_item_ids_to_owned_client_id[media_item["id"]]
+                is_owned = client_id == a
                 return is_owned
 
-            all_media_items = list(self.media_item_id_to_media_item.values())
+            all_media_items = list(self.__media_item_id_to_media_item.values())
             return list(filter(is_valid, all_media_items))
 
     def update_album(
         self, client_id, album_id, new_title=None, new_cover_media_item_id=None
     ):
-        if client_id != self.album_id_to_owned_client_id[album_id]:
+        if client_id != self.__album_id_to_owned_client_id[album_id]:
             raise Exception("Cannot update album it does not own")
 
-        album_info = self.album_id_to_album[album_id]
+        album_info = self.__album_id_to_album[album_id]
         if new_title is not None:
             album_info["title"] = new_title
         if new_cover_media_item_id is not None:
