2904c1ba10b582355494b8768356479081ce2434
Emilio K <emiliokartono@gmail.com>
2024-06-26 18:23:36 -0700

    fix: add more tests

--- tests/cleanup/test_gphotos_cleaner.py
+++ tests/cleanup/test_gphotos_cleaner.py
@@ -99,6 +99,26 @@ class GPhotosClientTests(unittest.TestCase):
         expected_media_item_ids = self.__get_media_ids_from_uploaded_files__(upload_1)
         self.assertEqual(media_item_ids_in_trash, expected_media_item_ids)
 
+    def test_mark_unalbumed_photos_to_trash__more_than_50_unalbumed_photos__puts_photos_to_trash(
+        self,
+    ):
+        # Test setup: Add 50 photos to an unshared album
+        uploads = []
+        for i in range(0, 100):
+            item = self.client_1.media_items().upload_photo(f"A/{i}.jpg", f"{i}.jpg")
+            upload = self.client_1.media_items().add_uploaded_photos_to_gphotos([item])
+            uploads += upload["newMediaItemResults"]
+
+        # Act:
+        cleaner = GPhotosCleaner(self.client_1)
+        cleaner.mark_unalbumed_photos_to_trash()
+
+        media_item_ids_in_trash = self.__get_media_item_ids_in_trash__(self.client_1)
+        expected_media_item_ids = self.__get_media_ids_from_uploaded_files__(
+            {"newMediaItemResults": uploads}
+        )
+        self.assertEqual(media_item_ids_in_trash, expected_media_item_ids)
+
     def __get_media_item_ids_in_trash__(self, client):
         albums = client.albums().list_albums()
         trash_id = next(x["id"] for x in albums if x["title"] == "Trash")
