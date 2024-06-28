28c51c8b84b01de2c25069d88654778aabd46675
Emilio K <emiliokartono@gmail.com>
2024-06-27 23:39:00 -0700

    feat: added event bus to gphotos cleaner

---
+++ sharded_google_photos/cleanup/events.py
@@ -0,0 +1,4 @@
+FOUND_TRASH_ALBUM = "cleanup:found_trash_album"
+CREATED_TRASH_ALBUM = "cleanup:created_trash_album"
+FOUND_ALBUMLESS_MEDIA_ITEMS = "cleanup:found_albumless_media_items"
+ADDED_ALBUMLESS_MEDIA_ITEMS_TO_TRASH = "cleanup:added_albumless_media_items_to_trash"
